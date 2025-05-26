21b6987e5a385d1c87a635a6ff120760a0f4de19
Raspoza <rogerioapmelojunior@gmail.com>
2025-05-25 23:11:02 -0300

    Raw Milk Buckets no longer heal.

--- src/main/java/raspoza/btarawmilk/mixin/Milked.java
+++ src/main/java/raspoza/btarawmilk/mixin/Milked.java
@@ -1,12 +1,14 @@
 package raspoza.btarawmilk.mixin;
 
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemBucket;
+import net.minecraft.core.item.ItemBucketIceCream;
 import net.minecraft.core.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.minecraft.core.item.Items.BUCKET;
 
 @Mixin(value = Items.class, remap = false)
 public class Milked {
@@ -14,6 +16,6 @@ public class Milked {
 	private static void replaceMilkBucket(CallbackInfo ci) {
 		Item.itemsList[Items.BUCKET_MILK.id] = null;
 		Item.itemsMap.remove(Items.BUCKET_MILK.namespaceID);
-		Items.BUCKET_MILK = new ItemBucket("rawMilkBucket", Items.BUCKET_MILK.namespaceID.toString(), Items.BUCKET_MILK.id, null);
+		Items.BUCKET_MILK = new ItemBucketIceCream("rawMilkBucket", Items.BUCKET_MILK.namespaceID.toString(), Items.BUCKET_MILK.id, 0, 0).setContainerItem(BUCKET);
 	}
 }
